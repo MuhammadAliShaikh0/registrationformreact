d51bbd5a63129dcd8441a291c95f512c6e434430
Muhammad Ali <159954209+MuhammadAliShaikh0@users.noreply.github.com>
2024-08-06 13:01:28 -0700

    Update Dashboard.jsx

--- routerdomlogin/src/Screens/Dashboard/Dashboard.jsx
+++ routerdomlogin/src/Screens/Dashboard/Dashboard.jsx
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import './Dashboard.css';
-
-// const Dashboard = () => {
-
-//   return (
-//     <div className="dashboard">
-//       <h1>Dashboard</h1>
-      
-    
-//     </div>
-//   );
-// }
-
-// export default Dashboard;
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Dashboard.css';
@@ -22,7 +6,7 @@ const Dashboard = () => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    // Perform logout operations like clearing any stored tokens or user info
+    
     navigate('/login');
   };
 
@@ -32,9 +16,7 @@ const Dashboard = () => {
         <h1>Welcome to the Dashboard</h1></header><br /><br />
         <button onClick={handleLogout} className="logout-button">Logout</button>
       
-      {/* <main className="dashboard-content"> */}
-        {/* <p>This is the dashboard. You can add more components and features here.</p> */}
-      {/* </main> */}
+    
     </div>
   );
 };
